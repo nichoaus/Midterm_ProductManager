43a187e26e0164b80a3e3282d03b374e9dbb66d6
Austin Nicholson <nichoaus@sheridancollege.ca>
2026-02-09 17:12:50 -0500

    Initial commit by Austin Nicholson

---
+++ src/main/java/midterm_nicholson/midterm_productmanager/Midterm_ProductManager.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package midterm_nicholson.midterm_productmanager;
+
+/**
+ *
+ * @author acsni
+ */
+public class Midterm_ProductManager {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
